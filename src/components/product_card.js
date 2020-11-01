e0e236b0bb04c98eb0cc71c6eae86bd648761e02
pierrewebdev <patrick.pierre000@gmail.com>
2020-11-01 17:23:12 -0500

    implemented feature in which an add to cart button will appear or disappear upon mousing in and out of a product card image

--- src/components/product_card.js
+++ src/components/product_card.js
@@ -1,17 +1,42 @@
 import React from "react";
 
-function ProductCard(props) {
-  return (
-    <div className="product-card">
-      <img
-        className="product-card-image"
-        src={props.image}
-        alt="One of our Fitness and Health Products"
-      />
-      <p className="product-card-name">{props.name}</p>
-      <span >${props.price}</span>
-    </div>
-  );
+class ProductCard extends React.Component {
+
+    state = {
+        showing:false
+    }
+
+  showMe = () =>{
+    return this.state.showing ? 
+    {visibility:"visible"} 
+        : 
+    {visibility:"hidden"}
+  }
+
+  swapBooleanState = () =>{
+    this.setState({showing: !this.state.showing})
+  }
+
+  addToCart = () =>{
+      //method is supposed to handle adding things to cart and persisting it
+  }
+  render(){
+    return (
+        <div onMouseEnter = {evt => this.swapBooleanState()} onMouseLeave = {evt => this.swapBooleanState()} className="product-card">
+          <div style = {{position:"relative",
+      width: "50%", maxWidth: "500px"}}>
+            <img
+              className="product-card-image"
+              src={this.props.image}
+              alt="One of our Fitness and Health Products"
+            />
+            <button onClick = {this.addToCart} style = {this.showMe()} className = "product-btn">Add to Cart</button>
+          </div>
+          <p className="product-card-name">{this.props.name}</p>
+          <span>${this.props.price}</span>
+        </div>
+      );
+  }
 }
 
 export default ProductCard;
