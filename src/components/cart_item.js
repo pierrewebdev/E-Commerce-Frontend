98135631840f6d60b8c90484f4f2f5c69c12a2e6
pierrewebdev <patrick.pierre000@gmail.com>
2020-11-12 10:22:41 -0500

    Improved the design of the shopping cart

--- src/components/cart_item.js
+++ src/components/cart_item.js
@@ -1,26 +1,37 @@
 import React from "react";
 
 function CartItem(props) {
+  const handleDelete = () => {
+    props.delete(props.productId);
+  };
 
-  const handleDelete = () =>{
-    props.delete(props.productId)
-  }
+  // const pTagStyles = {
+  //   fontSize: "16px",
+  //   width:"400px"
+  // };
 
   return (
-    <>
-      <div className = "cart-item">
+    <div className = "cart-container">
+      <div className="cart-item">
         <div>
-          <img className = "cart-img" src={props.image} alt="One of our Health and Fitness items" />
+          <img
+            className="cart-img"
+            src={props.image}
+            alt="One of our Health and Fitness items"
+          />
         </div>
         <div>
-          <p>{props.name}</p>
+          <p style = {{fontWeight:"bold"}}>{props.name}</p>
           <p>{props.description}</p>
-          <p>Price: ${props.price}</p>
-          <button onClick = {handleDelete}>Delet From Cart</button>
+        </div>
+        <div>
+          <p style = {{fontSize: "20px"}}>Price: ${props.price}</p>
+          <button className="review-button" onClick={handleDelete}>
+            Remove
+          </button>
         </div>
       </div>
-      <br/>
-    </>
+    </div>
   );
 }
 
